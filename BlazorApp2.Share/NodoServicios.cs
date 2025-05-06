ce3dd195d07c6b7b08cd301b7eeee9fa1ed7c09c
caarlb <s.caarlb@gmail.com>
2025-05-05 23:05:27 -0600

    inidio de servicios, agregar nodo en direccion indicada

---
+++ BlazorApp2.Share/NodoServicios.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Share
+{
+    public class NodoServicios
+    {
+        private Nodo NodoInicio;
+
+        public NodoServicios() 
+        { 
+            NodoInicio = null;
+        }
+
+        public void CrearNodoInicio(Nodo nodo)
+        {
+            NodoInicio = nodo;
+        }
+
+        public Nodo ObtenerNodoInicio()
+        {
+            return NodoInicio;
+        }
+
+        public void AgregarNodo(Nodo nodoReferencia, string direccion, Nodo nuevoNodo)
+        {
+            if (nodoReferencia == null)
+            {
+                Console.WriteLine("Error: El nodo de referencia no existe.");
+                return;
+            }
+
+            switch (direccion.ToLower())
+            {
+                case "arriba":
+                    if (nodoReferencia.ReferenciaArriba == null)
+                    {
+                        nodoReferencia.ReferenciaArriba = nuevoNodo;
+                        Console.WriteLine("Nodo agregado ARRIBA");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ya existe un nodo arriba.");
+                    }
+                    break;
+                case "abajo":
+                    if (nodoReferencia.ReferenciaAbajo == null)
+                    {
+                        nodoReferencia.ReferenciaAbajo = nuevoNodo;
+                        Console.WriteLine("Nodo agregado ABAJO");
+                    } else
+                    {
+                        Console.WriteLine("Ya existe un nodo abajo.");
+                    }
+                    break;
+                case "izquierda": 
+                    if (nodoReferencia.ReferenciaIzquierda == null)
+                    {
+                        nodoReferencia.ReferenciaIzquierda = nuevoNodo;
+                        Console.WriteLine("Nodo agregado IZQUIERDA");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ya existe un nodo izquierda.");
+                    }
+                    break;
+                case "derecha":
+                    if (nodoReferencia.ReferenciaDerecha == null)
+                    {
+                        nodoReferencia.ReferenciaDerecha = nuevoNodo;
+                        Console.WriteLine("Nodo agregado DERECHA");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ya existe un nodo derecha.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Dirección inválida. Indicar: arriba, abajo, izquierda o derecha.");
+                    break;
+
+            }
+        }
+    }
+}
